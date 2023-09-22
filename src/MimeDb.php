1524f055aebb15cb6c3a8fba393f4f5b88a3a624
ruff <ruff@thor>
2023-09-22 19:00:09 +0200

    Initial commit

---
+++ src/MimeDb.php
@@ -0,0 +1,126 @@
+<?php
+
+/*
+* This file is a part of horstoeko/mimedb.
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace horstoeko\mimedb;
+
+/**
+ * Class representing the mime repository
+ *
+ * @category MimeDb
+ * @package  MimeDb
+ * @author   D. Erling <horstoeko@erling.com.de>
+ * @license  https://opensource.org/licenses/MIT MIT
+ * @link     https://github.com/horstoeko/mimedb
+ */
+class MimeDb
+{
+    /**
+     * The internal mime database
+     *
+     * @var array
+     */
+    protected $mimeDatabase = [];
+
+    /**
+     * Load mime db
+     *
+     * @return MimeDb
+     */
+    protected function initializeDatabase(): MimeDb
+    {
+        if (!empty($this->mimeDatabase)) {
+            return $this;
+        }
+
+        $this->loadDatabase();
+
+        return $this;
+    }
+
+    /**
+     * Load database file
+     *
+     * @return MimeDb
+     */
+    protected function loadDatabase(): MimeDb
+    {
+        $this->mimeDatabase = json_decode(file_get_contents($this->getDbFilename()), true);
+        $this->mimeDatabase = array_filter($this->mimeDatabase, function ($mimetypeDefinition) {
+            return isset($mimetypeDefinition['extensions']) && !empty($mimetypeDefinition['extensions']);
+        });
+
+        return $this;
+    }
+
+    /**
+     * Find by file extension
+     *
+     * @param string $lookuoFileExtension
+     * @return string|null
+     */
+    public function findType(string $lookuoFileExtension): ?string
+    {
+        $this->initializeDatabase();
+
+        $foundMimeTypes = array_filter($this->mimeDatabase, function ($mimetypeDefinition) use ($lookuoFileExtension) {
+            return in_array(ltrim($lookuoFileExtension, "."), $mimetypeDefinition['extensions']);
+        });
+
+        if (count($foundMimeTypes) === 0) {
+            return null;
+        }
+
+        return array_keys($foundMimeTypes)[0];
+    }
+
+    /**
+     * Find by file extension
+     * This is an alias function for findTyoe
+     *
+     * @param string $lookuoFileExtension
+     * @return string|null
+     */
+    public function findByExtension(string $lookuoFileExtension): ?string
+    {
+        return $this->findType($lookuoFileExtension);
+    }
+
+    /**
+     * Find by mime type
+     *
+     * @param string $lookupMimeType
+     * @return string|null
+     */
+    public function findMimeType(string $lookupMimeType): ?string
+    {
+        $this->initializeDatabase();
+
+        $foundMimeTypes = array_filter($this->mimeDatabase, function ($mimetypeDefinition, $mimetype) use ($lookupMimeType) {
+            return strcasecmp($mimetype, $lookupMimeType) === 0;
+        }, ARRAY_FILTER_USE_BOTH);
+
+        if (reset($foundMimeTypes) === false) {
+            return null;
+        }
+
+        return current($foundMimeTypes)["extensions"][0];
+    }
+
+    /**
+     * Returns the full-qualified filename where
+     * the database is located
+     *
+     * @return string
+     * @codeCoverageIgnore
+     */
+    private function getDbFilename(): string
+    {
+        return dirname(__FILE__) . "/assets/mimetypes.json";
+    }
+}
---
+++ tests/testcases/MimeDbTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace horstoeko\mimedb\tests\testcases;
+
+use horstoeko\mimedb\MimeDb;
+use horstoeko\mimedb\tests\TestCase;
+
+class MimeDbTest extends TestCase
+{
+    /**
+     * Internal mime database
+     *
+     * @var \horstoeko\mimedb\MimeDb
+     */
+    protected $mimeDb = null;
+
+    /**
+     * @inheritDoc
+     */
+    protected function setUp(): void
+    {
+        $this->mimeDb = new MimeDb();
+    }
+
+    /**
+     * Constructor Test
+     *
+     * @return void
+     */
+    public function testConstruction(): void
+    {
+        $this->assertEmpty($this->getPrivatePropertyFromObject($this->mimeDb, 'mimeDatabase')->getValue($this->mimeDb));
+    }
+
+    /**
+     * Test "findType" method
+     *
+     * @covers \horstoeko\mimeDb\MimeDb::findType
+     * @covers \horstoeko\mimeDb\MimeDb::initializeDatabase
+     * @covers \horstoeko\mimeDb\MimeDb::loadDatabase
+     * @return void
+     */
+    public function testFindType(): void
+    {
+        $this->assertEquals('application/vnd.openxmlformats-officedocument.wordprocessingml.document', $this->mimeDb->findType('.docx'));
+        $this->assertEquals('video/mp4', $this->mimeDb->findType('.mp4'));
+        $this->assertEquals('audio/midi', $this->mimeDb->findType('.mid'));
+        $this->assertEquals('application/vnd.openxmlformats-officedocument.wordprocessingml.document', $this->mimeDb->findType('docx'));
+        $this->assertEquals('video/mp4', $this->mimeDb->findType('mp4'));
+        $this->assertEquals('audio/midi', $this->mimeDb->findType('mid'));
+        $this->assertNull($this->mimeDb->findType('.unknown'));
+    }
+
+    /**
+     * Test "findByExtension" method
+     *
+     * @covers \horstoeko\mimeDb\MimeDb::findByExtension
+     * @covers \horstoeko\mimeDb\MimeDb::initializeDatabase
+     * @covers \horstoeko\mimeDb\MimeDb::loadDatabase
+     * @return void
+     */
+    public function testFindByExtension(): void
+    {
+        $this->assertEquals('application/vnd.openxmlformats-officedocument.wordprocessingml.document', $this->mimeDb->findByExtension('.docx'));
+        $this->assertEquals('video/mp4', $this->mimeDb->findByExtension('.mp4'));
+        $this->assertEquals('audio/midi', $this->mimeDb->findByExtension('.mid'));
+        $this->assertEquals('application/vnd.openxmlformats-officedocument.wordprocessingml.document', $this->mimeDb->findByExtension('docx'));
+        $this->assertEquals('video/mp4', $this->mimeDb->findByExtension('mp4'));
+        $this->assertEquals('audio/midi', $this->mimeDb->findByExtension('mid'));
+        $this->assertNull($this->mimeDb->findByExtension('.unknown'));
+    }
+
+    /**
+     * Test "findMimeType" method
+     *
+     * @covers \horstoeko\mimeDb\MimeDb::findMimeType
+     * @covers \horstoeko\mimeDb\MimeDb::initializeDatabase
+     * @covers \horstoeko\mimeDb\MimeDb::loadDatabase
+     * @return void
+     */
+    public function testFindMimeType(): void
+    {
+        $this->assertEquals("docx", $this->mimeDb->findMimeType('application/vnd.openxmlformats-officedocument.wordprocessingml.document'));
+        $this->assertEquals("mp4", $this->mimeDb->findMimeType('video/mp4'));
+        $this->assertEquals("mid", $this->mimeDb->findMimeType('audio/midi'));
+        $this->assertNull($this->mimeDb->findMimeType('unknown/unknown'));
+    }
+}
